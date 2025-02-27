a0bc0eb948c16b95e57cdbeb9a7e870a0db051e4
jaymark17-web <libradojaymark_bscs@plmun.edu.ph>
2025-02-27 18:09:24 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,9 @@
+public class Greeting {
+    public static void main(String[] args) {
+        greet(); 
+    }
+    
+    public static String greet() {
+        return "Hello from the greet method!\nThis is my message!";
+    }
+}
